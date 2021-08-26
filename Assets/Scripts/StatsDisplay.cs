c3ada5b8a07dd3c8ae1635ac68ee3eb696853358
Bajtix <39443948+Bajtix@users.noreply.github.com>
2021-08-26 22:16:04 +0200

    speeeddd

--- Assets/Scripts/StatsDisplay.cs
+++ Assets/Scripts/StatsDisplay.cs
@@ -6,7 +6,7 @@ public class StatsDisplay : MonoBehaviour {
     public TextMeshProUGUI health;
 
     public void SetHealth(float health) {
-        this.health.TweenLocalScaleY(0.7f,0.05f).SetOnComplete(() => this.health.TweenLocalScaleY(1,0.05f));
+        this.health.TweenLocalScaleY(0.4f,0.05f).SetOnComplete(() => this.health.TweenLocalScaleY(1,0.05f));
         this.health.text = health.ToString("0.0");
         this.health.color = Color.Lerp(Color.red, Color.white, health/100);
     }
