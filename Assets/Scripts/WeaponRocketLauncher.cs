ed14b927f1ae1ea6e7b2166cb74b7b07bc35b8ce
Bajtix <bajtix.b@gmail.com>
2021-08-29 10:35:49 +0200

    rocket launcher

---
+++ Assets/Scripts/WeaponRocketLauncher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeLauncher : WeaponGun {
+
+    
+    public override void Fire1() {
+        base.Fire1();
+    }
+}
