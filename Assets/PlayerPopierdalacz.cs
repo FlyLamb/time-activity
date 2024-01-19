eddb84a40bb9011df78e075ba887669dbf182631
bajtix <39443948+Bajtix@users.noreply.github.com>
2024-01-19 23:02:34 +0100

    experimental player movement progress

---
+++ Assets/PlayerPopierdalacz.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(SphereCollider))]
+public class PlayerPopierdalacz : MonoBehaviour {
+    [SerializeField] private Rigidbody m_rb;
+    [SerializeField] private SpringJoint m_springJoint;
+    [SerializeField] private Transform m_groundPoint;
+
+    [Header("Ground")][SerializeField] private LayerMask m_mask = int.MaxValue;
+    [SerializeField] private float m_playerHeight, m_margin, m_rayOffset, m_radius;
+
+    [Header("Control")][SerializeField] private float m_acceleration;
+    [SerializeField] private float m_walkSpeed;
+
+
+    private void Start() {
+        m_springJoint.autoConfigureConnectedAnchor = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private float GetDistance(Vector3 atOffset) {
+        RaycastHit hit;
+        float dst = m_playerHeight + m_margin;
+        if (Physics.Raycast(transform.position + atOffset - transform.up * m_rayOffset, -transform.up, out hit, m_playerHeight + m_margin - m_rayOffset, m_mask)) {
+            dst = hit.distance + m_rayOffset;
+        }
+        Debug.DrawRay(transform.position + atOffset - transform.up * m_rayOffset, -transform.up * (m_playerHeight - m_rayOffset), Color.red, Time.fixedDeltaTime);
+        Debug.DrawRay(transform.position + atOffset - transform.up * m_playerHeight, -transform.up * m_margin, Color.blue, Time.fixedDeltaTime);
+        return dst;
+    }
+
+    private void FixedUpdate() {
+        float dst = 0;
+        dst += GetDistance(Vector3.forward * m_radius);
+        dst += GetDistance(Vector3.back * m_radius);
+        dst += GetDistance(Vector3.right * m_radius);
+        dst += GetDistance(Vector3.left * m_radius);
+        dst += GetDistance(Vector3.zero);
+        dst /= 5;
+        m_springJoint.connectedAnchor = transform.up * m_playerHeight;
+        m_groundPoint.position = transform.position - transform.up * dst;
+
+
+        ///
+
+        //var inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        var inputVector = new Vector3(
+            (Input.GetKey(KeyCode.D) ? 1f : 0f)
+            + (Input.GetKey(KeyCode.A) ? -1f : 0f),
+            0,
+            (Input.GetKey(KeyCode.W) ? 1f : 0f)
+            + (Input.GetKey(KeyCode.S) ? -1f : 0f)
+        ).normalized;
+        var velocity = new Vector3(m_rb.velocity.x, 0, m_rb.velocity.z);
+        var desiredVelocity = transform.TransformDirection(inputVector) * m_walkSpeed;
+
+        m_rb.AddForce(desiredVelocity - velocity, ForceMode.VelocityChange);
+    }
+}
--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -9,13 +9,13 @@ public class PlayerCamera : MonoBehaviour {
     private bool invertY;
 
     [SerializeField]
-    private BajtixPlayerController playerController;
+    private Rigidbody playerController;
 
     private Vector2 rotation;
     [SerializeField]
     private float minY = -90, maxY = 90, fovChangeSpeed = 5;
 
-[SerializeField]
+    [SerializeField]
     private AnimationCurve fovKick;
 
     private new Camera camera;
@@ -32,7 +32,7 @@ public class PlayerCamera : MonoBehaviour {
 
 
     private void Update() {
-        if(Cursor.lockState == CursorLockMode.None) return;
+        if (Cursor.lockState == CursorLockMode.None) return;
 
 
         Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
@@ -40,22 +40,22 @@ public class PlayerCamera : MonoBehaviour {
         rotation += input * sensitivity;
         rotation.y = Mathf.Clamp(rotation.y, minY, maxY);
 
-        playerController.transform.rotation = Quaternion.Euler(0,rotation.x, 0);
-        transform.localRotation = Quaternion.Euler(rotation.y, 0,0);
-        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView,fovKick.Evaluate(playerController.rb.velocity.magnitude) * 70,Time.deltaTime * fovChangeSpeed);
+        playerController.transform.rotation = Quaternion.Euler(0, rotation.x, 0);
+        transform.localRotation = Quaternion.Euler(rotation.y, 0, 0);
+        camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, fovKick.Evaluate(playerController.velocity.magnitude) * 70, Time.deltaTime * fovChangeSpeed);
 
-        if(Input.GetKeyDown(KeyCode.E)) {
+        if (Input.GetKeyDown(KeyCode.E)) {
             RaycastHit hit;
-            if(Physics.Raycast(transform.position, transform.forward, out hit,5, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore)) {
-                if(hit.collider.GetComponent<Interactable>())
-                hit.collider.GetComponent<Interactable>().Interact();
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 5, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore)) {
+                if (hit.collider.GetComponent<Interactable>())
+                    hit.collider.GetComponent<Interactable>().Interact();
             }
         }
     }
 
     private void FixedUpdate() {
-        if(shakeTime > 0) {
-            transform.localPosition = originalPoint + new Vector3(Random.Range(-shakeStr,shakeStr),Random.Range(-shakeStr,shakeStr),0);
+        if (shakeTime > 0) {
+            transform.localPosition = originalPoint + new Vector3(Random.Range(-shakeStr, shakeStr), Random.Range(-shakeStr, shakeStr), 0);
             shakeTime -= Time.fixedDeltaTime;
         } else transform.localPosition = originalPoint;
     }
