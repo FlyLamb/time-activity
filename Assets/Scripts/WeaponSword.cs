a318b22515206261cab20b8df866dc3b7f5fa46b
WalkanFL <maciej.b.szczepanski@o2.pl>
2024-01-22 23:23:57 +0100

    Animation Update

--- Assets/Scripts/WeaponSword.cs
+++ Assets/Scripts/WeaponSword.cs
@@ -1,27 +1,33 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class WeaponSword : Weapon {
+public class WeaponSword : Weapon
+{
     [Header("Sword Params")][SerializeField][FormerlySerializedAs("hitbox")] protected MeleeWeaponHitbox m_hitbox;
     [SerializeField][FormerlySerializedAs("damage")] private float m_damage;
     [SerializeField][FormerlySerializedAs("attackInterval")] private float m_attackCooldown;
     [SerializeField][FormerlySerializedAs("pushForce")] private float m_slashDashStrength = 3;
 
-    public override void Fire1() {
+    public override void Fire1()
+    {
         if (!ReadyToUse) return;
-        base.Fire1();
+        if (m_animator != null) m_animator.SetTrigger("Attack");
+        Manager.PlayAudio(m_fire1);
         Attack();
     }
 
-    public override void Fire2() {
+    public override void Fire2()
+    {
         if (!ReadyToUse) return;
-        base.Fire2();
+        if (m_animator != null) m_animator.SetTrigger("AttackAlt");
+        Manager.PlayAudio(m_fire1);
         Attack();
         Manager.PlayerController.rb
             .AddForce(Manager.CameraAim * m_slashDashStrength, ForceMode.Impulse);
     }
 
-    protected void Attack() {
+    protected void Attack()
+    {
         m_hitbox.Hit(m_damage, DamageType.Melee);
         m_cooldown = m_attackCooldown;
     }
