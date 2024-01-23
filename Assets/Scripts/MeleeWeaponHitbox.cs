bc3bf6b84e43af1b8882277976ef1036194b6f3f
WalkanFL <maciej.b.szczepanski@o2.pl>
2024-01-23 21:41:36 +0100

    Lightsaber fix/ Melee Weapon Hit Detection Fix

--- Assets/Scripts/MeleeWeaponHitbox.cs
+++ Assets/Scripts/MeleeWeaponHitbox.cs
@@ -2,26 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeWeaponHitbox : MonoBehaviour {
+public class MeleeWeaponHitbox : MonoBehaviour
+{
 
     protected List<Enemy> colliders = new List<Enemy>();
 
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.GetComponent<Enemy>())
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Enemy>())
             colliders.Add(other.GetComponent<Enemy>());
     }
 
-    private void OnTriggerExit(Collider other) {
-        if(other.GetComponent<Enemy>())
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Enemy>())
             colliders.Remove(other.GetComponent<Enemy>());
     }
 
-    public void Hit(float dmg, DamageType type = DamageType.Normal, float knockback = 1) {
+    public void Hit(float dmg, DamageType type = DamageType.Normal, float knockback = 1)
+    {
         colliders.RemoveAll((e) => e == null); // remove destroyed objects from the list
-        foreach(var w  in colliders) {
+        foreach (var w in colliders)
+        {
             w.Hit(dmg, type);
-            if(w.GetComponent<Rigidbody>()) w.GetComponent<Rigidbody>().AddForce(PlayerManager.Instance.controller.transform.forward * knockback, ForceMode.Impulse);
+            if (w.GetComponent<Rigidbody>()) w.GetComponent<Rigidbody>().AddForce(PlayerManager.Instance.controller.transform.forward * knockback, ForceMode.Impulse);
+            colliders.Remove(w.GetComponent<Enemy>());
         }
     }
 }
