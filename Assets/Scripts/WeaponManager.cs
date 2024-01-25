fd2dabe80764f75cc9c6465a09514dddce241b58
WalkanFL <maciej.b.szczepanski@o2.pl>
2024-01-25 17:27:28 +0100

    Full Scroll Wheel

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -2,9 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class WeaponManager : MonoBehaviour {
-    public static WeaponManager Instance {
-        get {
+public class WeaponManager : MonoBehaviour
+{
+    public static WeaponManager Instance
+    {
+        get
+        {
             if (m_instance == null)
                 m_instance = GameObject.FindObjectOfType<WeaponManager>();
             return m_instance;
@@ -25,36 +28,53 @@ public class WeaponManager : MonoBehaviour {
     private float delay = 0;
 
 
-    private void Start() {
-        if (GameManager.loadout != null) {
+    private void Start()
+    {
+        if (GameManager.loadout != null)
+        {
             weapons = GameManager.loadout;
         }
         Select(0);
     }
 
-    public void Select(int index) {
-        if (index < 0 || index >= weapons.Count) return;
+    public void Select(int index)
+    {
         if (delay > 0) return;
 
-        if (m_selectedWeapon != null) {
+        if (m_selectedWeapon != null)
+        {
             m_selectedWeapon.Hide();
         }
-        m_selected = index;
+        if (index < 0)
+        {
+            m_selected = weapons.Count;
+        }
+        else if (index >= weapons.Count)
+        {
+            m_selected = 0;
+        }
+        else
+        {
+            m_selected = index;
+        }
         m_selectedWeapon = Instantiate(weapons[m_selected].gameObject, transform).GetComponent<Weapon>();
         m_selectedWeapon.Show();
         GameObject.FindObjectOfType<WeaponDisplay>().ShowAnimation();
         delay = 0.2f;
-        if (!GameManager.unlocked.Contains(weapons[m_selected])) {
+        if (!GameManager.unlocked.Contains(weapons[m_selected]))
+        {
             GameManager.unlocked.Add(weapons[m_selected]);
         }
     }
 
-    public void PlayAudio(AudioClip clip) {
+    public void PlayAudio(AudioClip clip)
+    {
         if (clip == null) return;
         m_source.PlayOneShot(clip);
     }
 
-    private void Update() {
+    private void Update()
+    {
         if (delay >= 0)
             delay -= Time.deltaTime;
         if (m_selectedWeapon != null) m_selectedWeapon.WeaponUpdate();
@@ -62,14 +82,18 @@ public class WeaponManager : MonoBehaviour {
         if (Cursor.lockState == CursorLockMode.None) return;
         float sw = Input.GetAxis("Mouse ScrollWheel");
         switchWheelProgress -= sw;
-        if (switchWheelProgress > 0.02f) {
+        if (switchWheelProgress > 0.02f)
+        {
             Select(m_selected + 1);
             switchWheelProgress = 0;
-        } else if (switchWheelProgress < -0.02f) {
+        }
+        else if (switchWheelProgress < -0.02f)
+        {
             Select(m_selected - 1);
             switchWheelProgress = 0;
         }
-        if (m_selectedWeapon != null) {
+        if (m_selectedWeapon != null)
+        {
             if (Input.GetButton("Fire1")) m_selectedWeapon.Fire1();
             else
             if (Input.GetButton("Fire2")) m_selectedWeapon.Fire2();
